6679d1cf1ebcb64ef93fb3d5e0c5cc43be7e16ca
kvin1087 <kaustubh87@gmail.com>
2017-04-14 23:27:09 -0500

    Use of sets in es6

--- features/sets.js
+++ features/sets.js
@@ -1,4 +1,4 @@
-var chatRooms = new Set();
+/*var chatRooms = new Set();
 
 // Add items
 
@@ -7,6 +7,36 @@ chatRooms.add('runner');
 console.log(chatRooms.size);
 
 console.log( `Does the set has Runners: ${chatRooms.has('Runners')}`);
-chatRooms.delete('runner');
+/*chatRooms.delete('runner');
 console.log(chatRooms.size);
 chatRooms.clear();
+*/
+//To Array
+/*chatRooms.add('Kvin');
+console.log([...chatRooms]);
+
+//For Each on sets
+
+chatRooms.forEach(function(chatRoom){
+  console.log(`Found Chat room: ${chatRoom}` );
+});
+*/
+
+var Movie = new Set();
+
+function addMovie(movieTitle){
+  if(Movie.has(movieTitle)){
+    console.log(`Error movie already exist. Cannot add ${movieTitle}`);
+  }
+  else {
+    Movie.add(movieTitle);
+    console.log(`${movieTitle} has been added`);
+
+  }
+
+}
+
+
+addMovie('A new hope');
+addMovie('District 9');
+addMovie('A new hope');
